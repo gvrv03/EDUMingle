38a837dea8514971b66d33819fff643135fc5c4e
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-07-22 10:19:38 +0530

    Add Update User Fnc 10

--- src/helper/allLinks.js
+++ src/helper/allLinks.js
@@ -2,8 +2,8 @@ import baseUrl from "./baseUrl";
 
 // Auth
 export const Fast2SMSURL = "https://www.fast2sms.com/dev/bulkV2";
-export const SendSMSToUserURL = baseUrl + "api/auth/SendOTP";
-export const signInUserURL = baseUrl + "api/auth/SignInUser";
-export const checkUserExistURL = baseUrl + "api/auth/CheckUserExists";
-export const RegisterUserURL = baseUrl + "api/auth/Register";
-export const checkUserURL = baseUrl + "api/auth/CheckLogin";
+export const SendSMSToUserURL = baseUrl + "api/Auth/SendOTP";
+export const signInUserURL = baseUrl + "api/Auth/SignInUser";
+export const checkUserExistURL = baseUrl + "api/Auth/CheckUserExists";
+export const RegisterUserURL = baseUrl + "api/Auth/Register";
+export const checkUserURL = baseUrl + "api/Auth/CheckLogin";
