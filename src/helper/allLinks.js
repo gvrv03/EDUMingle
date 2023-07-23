c205f685352a8e7eed8600869b77873115052d4e
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-07-23 09:08:01 +0530

    convert axios to fetch remove api from Auth

--- src/helper/allLinks.js
+++ src/helper/allLinks.js
@@ -2,8 +2,8 @@ import baseUrl from "./baseUrl";
 
 // Auth
 export const Fast2SMSURL = "https://www.fast2sms.com/dev/bulkV2";
-export const SendSMSToUserURL = baseUrl + "api/Auth/SendOTP";
-export const signInUserURL = baseUrl + "api/Auth/SignInUser";
-export const checkUserExistURL = baseUrl + "api/Auth/CheckUserExists";
-export const RegisterUserURL = baseUrl + "api/Auth/Register";
-export const checkUserURL = baseUrl + "api/Auth/CheckLogin";
+export const SendSMSToUserURL = baseUrl + "api/SendOTP";
+export const signInUserURL = baseUrl + "api/SignInUser";
+export const checkUserExistURL = baseUrl + "api/CheckUserExists";
+export const RegisterUserURL = baseUrl + "api/Register";
+export const checkUserURL = baseUrl + "api/CheckLogin";
