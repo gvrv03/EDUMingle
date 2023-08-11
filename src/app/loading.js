42a5ad444dcc1737620f2974f9d70e8dbec04b00
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2023-08-11 15:02:18 +0530

    Fix some navbar errors 2

--- src/app/loading.js
+++ src/app/loading.js
@@ -4,15 +4,17 @@ import React from "react";
 
 const Alloading = () => {
   return (
-    <div
-      className="grid bottom-0 place-items-center w-full h-[100vh] left-0
+    <>
+      <NavBar shadow="drop-shadow-md" position="relative" />
+      <div
+        className="grid bottom-0 place-items-center w-full h-[100vh] left-0
      bg-white top-0 "
-    >
-      <NavBar shadow="drop-shadow-md" position="fixed" />
-      <FullScreenLoader />
+      >
+        <FullScreenLoader />
 
-      {/* <LoadingSpinner /> */}
-    </div>
+        {/* <LoadingSpinner /> */}
+      </div>
+    </>
   );
 };
 
