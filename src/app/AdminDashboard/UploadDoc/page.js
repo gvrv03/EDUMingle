67778cc0557ec6fde122c22951558718f2cdd3f6
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-01-06 22:24:11 +0530

    Addd image upload in firebase and app product done 3

--- src/app/AdminDashboard/UploadDoc/page.js
+++ src/app/AdminDashboard/UploadDoc/page.js
@@ -1,12 +1,10 @@
 "use client";
-import PdfViewer from "@/components/PdfViewer";
 import React from "react";
 import { useState } from "react";
 const page = () => {
   return (
     <div className=" bg-white p-5  flex-col flex gap-5  ">
       <h6>Upload Documents</h6>
-      <PdfViewer pdfUrl="https://pdfobject.com/pdf/sample.pdf" />
     </div>
   );
 };
