ac7024eb64e0e7afac731fcb0c758072fdcfcd3d
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-01-13 13:45:30 +0530

    add tutorial section 2

--- src/app/Tutorial/page.js
+++ src/app/Tutorial/page.js
@@ -6,7 +6,7 @@ import React from "react";
 import { Suspense } from "react";
 import { useState } from "react";
 
-const page = () => {
+const Page = () => {
   const [page, setpage] = useState(1);
 
   return (
@@ -19,4 +19,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default Page;
