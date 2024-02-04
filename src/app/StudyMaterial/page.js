987afcb6682ad5a1eb9fde12cfdafcdb1b1cca05
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-02-04 16:36:08 +0530

    remove  pdf view featur 3

--- src/app/StudyMaterial/page.js
+++ src/app/StudyMaterial/page.js
@@ -1,11 +1,7 @@
 import React from "react";
 
-function Home() {
+const StudyMaterial = () => {
+  return <div>Study Material</div>;
+};
 
-
-  return (
-    <div>
-      
-    </div>
-  );
-}
+export default StudyMaterial;
