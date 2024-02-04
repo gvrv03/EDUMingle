48f45d90287788a6025d674edf525b52fab31656
Gaurav Narnaware <itsgaurav3112003@gmail.com>
2024-02-04 16:30:26 +0530

    remove  pdf view feature

--- src/app/StudyMaterial/page.js
+++ src/app/StudyMaterial/page.js
@@ -1,23 +1,11 @@
-import dynamic from "next/dynamic";
-// const CodeBlock = dynamic(() => import("../../Components/Utility/CodeBlock"), {
-//   ssr: false,
-//   loading: () => <p>Loading</p>,
-// });
-import { Document, Page } from 'react-pdf';
-export default function Home() {
+import React, { useState } from "react";
+
+function Home() {
+
+
   return (
-    <div className="w-full h-screen">
-      {/* <iframe
-        src="https://gvrvnarnaware.vercel.app/GauravCV.pdf"
-        frameborder="0"
-        className="w-[50%] h-full"
-      /> */}
-      <div>
-        <Document file="https://gvrvnarnaware.vercel.app/GauravCV.pdf">
-          <Page pageNumber={1} />
-        </Document>
-      </div>
-      {/* <CodeBlock gistid="9d2bd56a7da1b2ec186009e695bd7fc5" /> */}
+    <div>
+      
     </div>
   );
 }
